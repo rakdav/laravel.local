3636b47b17c1e42ed085850d5ff10fc55859516a
rakdav <rakdav@inbox.ru>
2026-05-04 10:51:00 +0200

    api.route

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,10 @@
 <?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(ProductController::class)->
+    prefix('products')->group(function () {
+        Route::get('', 'index')->name('products.index') ;
+        Route::get('{product}', 'show')->name('products.show') ;
+});
